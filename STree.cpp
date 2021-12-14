48a578b575670c499d18e14cadeb560c90032b32
Mrrrrrrrrrrr <96118055+Mrrrrrrrrrrr@users.noreply.github.com>
2021-12-14 20:11:45 +0800

    Add files via upload

---
+++ STree.cpp
@@ -0,0 +1,64 @@
+#include "STree.h"
+
+Status InitSTree(STree& ST, const string& data)
+{
+    ST = new STNode();
+    ST->parent = ST;
+    ST->data = data;
+    ST->nextsibling = NULL;
+    ST->children = NULL;
+    return OK;
+}
+
+Status Insertchild(STree& ST, const string& data)
+{
+    if (!ST->children)
+    {
+        STree p = new STNode();
+        p->parent = ST;
+        p->data = data;
+        p->nextsibling = NULL;
+        p->children = NULL;
+        ST->children = p;
+        return OK;
+    }
+    else
+    {
+        STree p = ST->children;
+        while (p->nextsibling)
+            p = p->nextsibling;
+        STree q = new STNode();
+        q->parent = ST;
+        q->data = data;
+        q->children = NULL;
+        q->nextsibling = NULL;
+        p->nextsibling = q;
+        return OK;
+    }
+}
+
+Status DeleteNode(STree& ST)
+{
+    if (ST->parent = ST)
+    {
+        STree p = ST;
+        ST = ST->nextsibling;
+        free(p);
+        return OK;
+    }
+    else if (ST->parent->children = ST)
+    {
+        ST->parent->children = ST->nextsibling;
+        free(ST);
+        return OK;
+    }
+    else
+    {
+        STree p = ST->parent->children;
+        while (p->nextsibling != ST)
+            p = p->nextsibling;
+        p->nextsibling = ST->nextsibling;
+        free(ST);
+        return OK;
+    }
+}
