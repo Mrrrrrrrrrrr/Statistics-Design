d118d5eb1f704a3b71ec144246750c6fe575c873
Mrrrrrrrrrrr <96118055+Mrrrrrrrrrrr@users.noreply.github.com>
2021-12-14 21:36:26 +0800

    Update STree.cpp

--- STree.cpp
+++ STree.cpp
@@ -1,7 +1,5 @@
 #include "STree.h"
 
-cout<<"mrrrrrrrrrrr"<<endl;
-
 Status InitSTree(STree& ST, const string& data)
 {
     ST = new STNode();
@@ -63,4 +61,48 @@ Status DeleteNode(STree& ST)
         free(ST);
         return OK;
     }
+}
+
+// 创建一个函数，使其能够在结点的孩子们中寻找与给定值相同的孩子，返回其指针，若未找到则返回NULL
+STree FindChild(const STree& ST, const string& site)
+{
+    auto q = ST->children;
+    while (q->data != site && q->nextsibling)
+        q = q->nextsibling;
+    if (q->data == site)
+        return q;
+    else
+        return NULL;
+}
+
+void PushSite(const string& site, STree& ST)
+{
+    stack<string> s;  // 创建一个用于存储string类型的stack用作临时的存储器
+    auto iter1 = site.begin();  // 一号迭代器指示每一段域名的首个字母
+    auto iter2 = site.begin();  // 二号迭代器在域名压栈时指示其后的"."
+    string part[4];
+    int i = 0; // i用以计入段数
+    while (!(iter1 == iter2 && iter2 == site.end()))
+    {
+        while (iter2 != site.end() && *iter2 != '.')
+            ++iter2;
+        for (iter1; iter1 != iter2; ++iter1)
+        {
+            part[i] += *iter1;
+        }
+        if (iter2 != site.end())
+        {
+            ++iter1;
+            ++iter2;
+        }
+        s.push(part[i]);
+        ++i;
+    }
+    auto p = ST;
+    while (!s.empty())
+    {
+        Insertchild(p, s.top());
+        p = FindChild(p, s.top());
+        s.pop();
+    } // 将临时存储栈中的元素逐个添加入域名树中
 }
