52b0262350d38a48ef270f658a3607c4bf7f66fc
Mrrrrrrrrrrr <96118055+Mrrrrrrrrrrr@users.noreply.github.com>
2021-12-18 02:58:44 +0800

    Update running.cpp

--- running.cpp
+++ running.cpp
@@ -1,14 +1,9 @@
+#include "STree.h"
+#include <fstream>
 #include <iostream>
 #include <string>
-#include "STree.h"
 
 int main()
 {
-	STree ST;
-	InitSTree(ST, "ym");
-	PushSite("www.baidu.com", ST);
-	PushSite("www.baidu", ST);
-	PushSite("www.wyy.com", ST);
-	FindLeaf(ST);
-	cout << AllLeaf.size() << endl;
+	Menu();
 }
